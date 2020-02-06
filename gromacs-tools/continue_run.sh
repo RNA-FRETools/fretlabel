f2a37383c85cc1a03eb5c3fbce7e1c5a7743a86d
Fabio Steffen <35061965+fdsteffen@users.noreply.github.com>
2020-02-07 00:50:48 +0100

    Fix continue_run.sh

--- gromacs-tools/continue_run.sh
+++ gromacs-tools/continue_run.sh
@@ -4,10 +4,10 @@
 
 # cmd parsing functions
 usage() { echo "Continue a stopped run 
-Usage: continue_run.sh -s <run input file (.tpr)> -c <prev. checkpoint file>
+Usage: continue_run.sh -s <prev. run input file (.tpr)> -c <prev. checkpoint file>
 
 or extend a finished run
-Usage: continue_run.sh -s <run input file (.tpr)> -c <prev. checkpoint file> -e <timetoextend (ps)> -o <extension run input file (.tpr)>" 1>&2; exit 1; }
+Usage: continue_run.sh -s <prev. run input file (.tpr)> -c <prev. checkpoint file> -e <timetoextend (ps)> -o <extension run input file (.tpr)>" 1>&2; exit 1; }
 invalidOpt() { echo "Invalid option: -$OPTARG" 1>&2; exit 1; }
 missingArg() { echo "Option -$OPTARG requires an argument" 1>&2; exit 1; }
 cleanup() { if ls -f $1/\#* 1> /dev/null 2>&1 ; then rm $1/\#* ; fi ; }
@@ -16,7 +16,7 @@ cleanup() { if ls -f $1/\#* 1> /dev/null 2>&1 ; then rm $1/\#* ; fi ; }
 # cmd parsing
 #------------
 
-while getopts ":s:c:h" opt; do
+while getopts ":s:c:e:o:h" opt; do
     case $opt in
         s) 
             inputFile=$OPTARG
@@ -56,7 +56,7 @@ inputName=`echo $inputFile | rev | cut -f1 -d"/" | rev | cut -f1 -d"."`
 
 if [ -z "$timetoextend" ]; then
     # continue interrupted run
-    gmx mdrun -v -s md0/"$inputName".tpr -cpi md0/"$inputName".cpt -x md0/"$inputName".xtc -e md0/"$inputName".edr -g md0/"$inputName".log || { echo "-> Error: gmx mdrun for md0 restart failed" ; cleanup md0 ; exit 1; }
+    gmx mdrun -v -s md0/"$inputName".tpr -cpi md0/"$inputName".cpt -x md0/"$inputName".xtc -e md0/"$inputName".edr -g md0/"$inputName".log -append || { echo "-> Error: gmx mdrun for md0 restart failed" ; cleanup md0 ; exit 1; }
 else
     # extend finished run
     if [ -o "$extInputFile" ]; then
