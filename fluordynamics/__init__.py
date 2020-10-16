bc5ed115160c9382c25b0f3f55400cee7e81dc68
Fabio Steffen <35061965+fdsteffen@users.noreply.github.com>
2020-10-16 13:08:41 +0200

    Fix imports in tests

--- fluordynamics/__init__.py
+++ fluordynamics/__init__.py
@@ -2,6 +2,3 @@ from fluordynamics import ff
 from fluordynamics import jupyter
 from fluordynamics import fluorburst
 import os
-
-_SRC_DIR = os.path.abspath(os.path.dirname(__file__))
-_TEST_DIR = os.path.join(_SRC_DIR, 'tests')
