a7a6cd29c568837b9871d0a0cc06265d8f10239a
Fabio Steffen <35061965+fdsteffen@users.noreply.github.com>
2020-10-17 18:27:39 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,6 +13,7 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../../'))
+os.environ['PYTHONPATH'] = ':'.join((sys.path, os.environ.get('PYTHONPATH', '')))
 
 # -- Project information -----------------------------------------------------
 
